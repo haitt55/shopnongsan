f6caa47a99f35bda7e28dec2a08eb7c71346fcaa
Giang Thai Cuong <thaicuong.giang@gmail.com>
2015-11-09 00:20:38 +0700

    Categories listing

---
+++ database/migrations/2015_11_08_083700_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id')->default(0);
+            $table->string('name');
+            $table->tinyInteger('type');
+            $table->string('page_title', 70)->nullable();
+            $table->string('meta_keyword')->nullable();
+            $table->string('meta_description', 160)->nullable();
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+}
---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.master')
+
+@section('title', 'List Categories')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Categories</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Listing Categories
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            
+                        </div>
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+@endsection
